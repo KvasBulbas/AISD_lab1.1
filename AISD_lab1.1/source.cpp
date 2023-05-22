227745ce991b971fd00b50dedb74ae0df623a5d8
kvasbulbas <shamonin.2002@mail.ru>
2023-05-22 21:46:42 +0700

    Создание, заполнение, проверка на упорядоченность и на пустоту массива
    в файле

--- AISD_lab1.1/source.cpp
+++ AISD_lab1.1/source.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
+#include "CreateFile.h"
+#include<iostream>
+#include<fstream>
+#include<ctime>
+#include<random>
+
 
 int main()
 {
-	int a = 6;
-	std::cout << a + 5;
-	
+	const int numbersCount = 100;
+	const int maxNumberValue = 100;
+	std::string fileName = "Z:\\fileSorts\\straightSort2.txt";
+	createFileWithRandomNumbers(fileName, numbersCount, maxNumberValue);
+
+	std::cout<< isempty(fileName) << " " << isFileContainsSortedArray(fileName);
+
 	return 0;
 }
