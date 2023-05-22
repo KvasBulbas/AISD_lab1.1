c5bf23b5dd07ad30352214fadc09619a83520731
kvasbulbas <shamonin.2002@mail.ru>
2023-05-22 22:07:24 +0700

    add merge function

--- AISD_lab1.1/StraightSoirt.h
+++ AISD_lab1.1/StraightSoirt.h
@@ -28,3 +28,66 @@ void split(std::string fileName, std::string fileB, std::string fileC, int p)
 	}
 	A.close(); B.close(); C.close();
 }
+
+
+void merge(std::string fileName, std::string fileB, std::string fileC, int p)
+{
+
+	int x, y;
+	std::ofstream A(fileName);
+	std::ifstream B(fileB);
+	std::ifstream C(fileC);
+	B >> x; C >> y;
+
+	while (!(B.eof()) && !(C.eof()))
+	{
+
+		int i = 0, j = 0;
+
+		while (!(B.eof()) && !(C.eof()) && i < p && j < p)
+		{
+			if (x < y)
+			{
+				A << x << " ";
+				B >> x;
+				i++;
+			}
+			else
+			{
+				A << y << " ";
+				C >> y;
+				j++;
+			}
+		}
+
+		while (!(B.eof()) && i < p)
+		{
+			A << x << " ";
+			B >> x;
+			i++;
+		}
+
+		while (!(C.eof()) && j < p)
+		{
+			A << y << " ";
+			C >> y;
+			j++;
+		}
+	}
+
+
+	while (!(B.eof()))
+	{
+		A << x << " ";
+		B >> x;
+	}
+
+	while (!(C.eof()))
+	{
+		A << y << " ";
+		C >> y;
+	}
+
+	A.close(); B.close(); C.close();
+
+}
